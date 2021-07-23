9388ba2010eac7e074dd866fa0297341e562d913
Elliott Botwick <74318276+ddl-botwick@users.noreply.github.com>
2021-07-23 12:13:08 -0400

    Update write_to_artifacts.py

--- write_to_artifacts.py
+++ write_to_artifacts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import datetime
 df = pd.DataFrame([["time","is", datetime.datetime.now()]], columns = ['a', 'b', 'c'])
+print('branch4')
 print(df)
 df.to_csv('/mnt/artifacts/fileInArtifacts.csv')
